833ab476d3fd4e4fca4e1d3de35f3bcfbb5dc9bf
yoshidaibuki <yoshidaibuki@yoshidaibukinoMacBook-Air.local>
2024-06-07 22:22:00 +0900

    recipiapp.pyに戻した

--- apps/authapp/views.py
+++ apps/authapp/views.py
@@ -12,7 +12,7 @@ from flask_login import login_user
 from sqlalchemy import select
 from apps.authapp import forms
 from apps import models
-from apps.app import db
+from apps.recipiapp import db
 
 @authapp.route('/login',methods=['GET','POST'])
 def index():
--- apps/models.py
+++ apps/models.py
@@ -4,7 +4,7 @@ from werkzeug.security import generate_password_hash,check_password_hash
 
 from flask_login import UserMixin
 
-from apps.app import db
+from apps.recipiapp import db
 
 class User(db.Model,UserMixin):
     __tablename__="users"
